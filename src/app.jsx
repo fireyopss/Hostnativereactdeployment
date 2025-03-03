42943f9ae4f94e4eb4c349981cc8f9de3e6d178a
Tayyab <tayyab.javed@voneus.com>
2025-03-03 23:31:02 +0000

    push

--- src/app.jsx
+++ src/app.jsx
@@ -1,1 +1,10 @@
 import React from "react";
+import ReactDOM from "react-dom";
+
+export default function App() {
+    return <h1>Hello, world!, this is a react app</h1>;
+}
+
+
+
+ReactDOM.render(<App />, document.getElementById("app"));
