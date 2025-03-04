168a24c7aa28532225c08fc1b3e1d422fad0f563
MrOrangeade <tayyabdev@outlook.com>
2025-03-04 11:59:29 +0000

    change ip

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 export default function App() {
-    return <h1>this is a basic react vite project... hello world this is working v8</h1>;
+    return <h1>this is a basic react vite project... hello world this is working v9</h1>;
 }
 
 
