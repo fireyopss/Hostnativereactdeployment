a3daf442e139a2c04d5fc134a6e49a71cb5b881b
MrOrangeade <tayyabdev@outlook.com>
2025-03-05 19:19:33 +0000

    put s3 backend

--- .infra/main.tf
+++ .infra/main.tf
@@ -1,4 +1,17 @@
 terraform {
+
+  backend "s3" {
+
+    bucket = "tayyabjavedstate"
+
+    key = "hostnative"
+
+    region = "eu-west-1"
+
+
+
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -11,7 +24,7 @@ variable "aws_access_key" {
   type = string
 }
 
-variable "aws_secret_key"{
+variable "aws_secret_key" {
   type = string
 }
 
@@ -26,37 +39,37 @@ resource "aws_key_pair" "my_key" {
   public_key = file("~/.ssh/id_rsa.pub")
 }
 
-resource "aws_security_group" "my_sg"{
-    name        = "my_sg"
-    description = "Allow HTTP and SSH inbound traffic"
-     
-    ingress {
-        from_port   = 22
-        to_port     = 22
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+resource "aws_security_group" "my_sg" {
+  name        = "my_sg"
+  description = "Allow HTTP and SSH inbound traffic"
 
-    ingress {
-        from_port   = 80
-        to_port     = 80
-        protocol    = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-06e02ae7bdac6b938"
-  instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name
-    security_groups = [aws_security_group.my_sg.name]
+  ami             = "ami-06e02ae7bdac6b938"
+  instance_type   = "t2.micro"
+  key_name        = aws_key_pair.my_key.key_name
+  security_groups = [aws_security_group.my_sg.name]
 
   tags = {
     Name = "react-website"
